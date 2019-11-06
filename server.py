02c15ca96d9b56aff67dad90409a2649603bc9d0
Ke <kesun5@illinois.edu>
2019-11-06 15:23:35 -0600

    multiple file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import os
 from threading import Thread
 from SocketServer import ThreadingMixIn
 
@@ -19,18 +20,32 @@ class ClientThread(Thread):
         print " New thread started for "+ip+":"+str(port)
 
     def run(self):
-        filename='/home/pi/Downloads/cloud-formation-clouds-cloudy-247478.jpg'
-        f = open(filename,'rb')
-        while True:
-            l = f.read(BUFFER_SIZE)
-            while (l):
-                self.sock.send(l)
-                #print('Sent ',repr(l))
-                l = f.read(BUFFER_SIZE)
-            if not l:
-                f.close()
-                self.sock.close()
-                break
+        Path = "/home/pi/Downloads/"
+        filelist = os.listdir(Path)
+        for i in filelist:
+            if i.endswith(".jpg"):  # You could also add "and i.startswith('f')
+                with open(Path + i, 'r') as f:
+                    while True:
+                        l = f.read(BUFFER_SIZE)
+                        while (l):
+                            self.sock.send(l)
+                            l = f.read(BUFFER_SIZE)
+                        if not l:
+                            f.close()
+                            self.sock.close()
+                            break
+        #filename='/home/pi/Downloads/cloud-formation-clouds-cloudy-247478.jpg'
+        #f = open(filename,'rb')
+        #while True:
+         #   l = f.read(BUFFER_SIZE)
+          #  while (l):
+           #     self.sock.send(l)
+            #    #print('Sent ',repr(l))
+            #    l = f.read(BUFFER_SIZE)
+            #if not l:
+             #   f.close()
+              #  self.sock.close()
+               # break
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
