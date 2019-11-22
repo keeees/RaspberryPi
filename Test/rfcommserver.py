fdbe4712fe21ed5d7daf34cd4d0a58fc2cf04e73
ke <kesun5@illinois.edu>
2019-11-21 18:14:46 -0600

    add file transfer protocol for bluetooth

--- Test/rfcommserver.py
+++ Test/rfcommserver.py
@@ -1,11 +1,17 @@
-# file: rfcomm-server.py
-# auth: Albert Huang <albert@csail.mit.edu>
-# desc: simple demonstration of a server application that uses RFCOMM sockets
-#
-# $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
 
+import time
 from bluetooth import *
-
+MAXBUFLEN = 1024
+def readn(sock, count):
+    data = b''
+    while len(data) < count:
+        packet = sock.recv(count - len(data))
+        if packet == '':
+            return ''
+        data += packet
+    #size_buff = data
+    return data
+    
 server_sock=BluetoothSocket( RFCOMM )
 server_sock.bind(("",PORT_ANY))
 server_sock.listen(1)
@@ -17,10 +23,10 @@ uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 advertise_service( server_sock, "SampleServer",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
+                   profiles = [ SERIAL_PORT_PROFILE ],
+#                   protocols = [ OBEX_UUID ]
                     )
-                   
+
 print("Waiting for connection on RFCOMM channel %d" % port)
 
 client_sock, client_info = server_sock.accept()
@@ -28,9 +34,34 @@ print("Accepted connection from ", client_info)
 
 try:
     while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print("received [%s]" % data)
+        count = server_sock.recv(MAXBUFLEN)
+        num_files = int.from_bytes(count, byteorder='big')
+        print('Number of files to be received', int.from_bytes(count, byteorder='big'))
+        #time_start = time.clock()
+        for i in range(num_files):
+            #while(readn(server_sock,4)!=''):
+            size_buff = readn(server_sock, 4)
+            if size_buff == '':
+                print('Failed to receive file size.', file=sys.stderr)
+                server_sock.close()
+                sys.exit(3)
+
+            size_unpacked = struct.unpack('!I', size_buff)
+            file_size = size_unpacked[0]
+            print('Will receive file of size', file_size, 'bytes.')
+
+            with open('received_file_%i.jpg' %i, 'wb') as f:
+                BUFFER = MAXBUFLEN
+                while file_size > 0:
+                    if(file_size<BUFFER):
+                        BUFFER=file_size
+                    data = server_sock.recv(BUFFER)
+                    #print(len(data), 'bytes received.')
+                    if not data:
+                        print('End of  file.',i)
+                        break
+                    f.write(data)
+                    file_size -= len(data)
 except IOError:
     pass
 
